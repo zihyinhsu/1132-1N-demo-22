0d3b2a2c369bbbad1156203c4d277636a4fcabd9
Zihyin <jenny010328@gmail.com>
2025-02-20 22:53:29 +0800

    W01-P4: implement subtract button

--- demo/w01_dom_22/js/w01_22.js
+++ demo/w01_dom_22/js/w01_22.js
@@ -1,29 +1,41 @@
 const userInput = document.querySelector('.input-number');
 const addBtn = document.querySelector('.btn-add');
-
+const subtractBtn = document.querySelector('.btn-subtract');
 const currentResultOutput = document.querySelector('.current-result');
 const currentCalculationOutput = document.querySelector('.current-calculation');
 
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-console.log('userInput',userInput);
-console.log('addBtn',addBtn);
-console.log('currentResultOutput',currentResultOutput);
+// console.log('userInput',userInput);
+// console.log('addBtn',addBtn);
+// console.log('currentResultOutput',currentResultOutput);
 
 function getUserInput(){
     return parseInt(userInput.value);
 }
 
 function add(){
-
-const operand1 = currentResult;
-const operand2 = getUserInput();
-currentResult = operand1 + operand2;
-currentResultOutput.textContent = currentResult;
-const calText = `${operand1} + ${operand2}`;    
-console.log('calText: ',calText);
-currentCalculationOutput.textContent = calText;
+    const operand1 = currentResult;
+    const operand2 = getUserInput();
+    currentResult = operand1 + operand2;
+    currentResultOutput.textContent = currentResult;
+    const calText = `${operand1} + ${operand2}`;    
+    console.log('calText: ',calText);
+    currentCalculationOutput.textContent = calText;
 }
 
 addBtn.addEventListener('click',add);
+
+function subtract(){
+    const operand1 = currentResult;
+    const operand2 = getUserInput();
+    currentResult = operand1 - operand2;
+    currentResultOutput.textContent = currentResult;
+    const calText = `${operand1} - ${operand2}`;    
+    console.log('calText: ',calText);
+    currentCalculationOutput.textContent = calText;
+}
+
+subtractBtn.addEventListener('click',subtract);
+

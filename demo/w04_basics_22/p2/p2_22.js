7e5b54408d54d625170fdf63e1069c824dcd8cfe
Zihyin <jenny010328@gmail.com>
2025-03-17 21:31:30 +0800

    W04-P4: Show toSorted data and a stat object to store statistical data

--- demo/w04_basics_22/p2/p2_22.js
+++ demo/w04_basics_22/p2/p2_22.js
@@ -60,3 +60,41 @@ const outputStat1 = (s) => {
 // sdata.push(85);
 
 outputStat1(sdata);
+
+const toSorted = sdata.toSorted((a,b) => b - a );
+
+console.log("sdata after sorting: ", sdata);
+console.log("toSorted", toSorted);
+
+let state = {
+    pass: 0,
+    fail: 0,
+    sum: 0,
+    average: 0
+};
+
+const outputStat2 = (s) => {
+    result1.innerHTML = `
+        <h3 class="my-4">Array Staticistics</h3>
+        <p>sdata original:</p>
+        <p>${JSON.stringify(s)}</p>
+        <p>Total: ${s.length}</p>
+        <p>Pass: ${computePass(s)}</p>
+        <p>Fail: ${computeFail(s)}</p>
+        <p>Heightest: ${s[0]}</p>
+        <p>Lowest: ${s[s.length - 1]}</p>
+        <p>Average: ${computeAverage(s)}</p>
+    `;
+};
+
+const computeStat = (s) => {
+    s.forEach((score)=>{
+        if(score >= 59.5) state.pass++;
+        else state.fail++;
+        state.sum += score;
+        state.average = state.sum / s.length
+    })
+};
+
+computeStat(toSorted);
+outputStat2(toSorted);

282bd242974ab1a63b94949a1149b059b3b9fe02
Zihyin <jenny010328@gmail.com>
2025-04-13 16:19:13 +0800

    add quiz_exam

---
+++ quiz_exam/quiz1_xx/p2_xx/src/data_xx.js
@@ -0,0 +1,1 @@
+export const data1 = [19, 28, 37, 46, 55];
---
+++ quiz_exam/quiz1_xx/p4_xx/data_xx.js
@@ -0,0 +1,54 @@
+const menu_xx = [
+  {
+    id: 1,
+    title: 'buttermilk pancakes',
+    category: 'breakfast',
+    price: 15.99,
+    img: './images/item-1.jpeg',
+    desc: "I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed",
+  },
+  {
+    id: 2,
+    title: 'diner double',
+    category: 'lunch',
+    price: 13.99,
+    img: './images/item-2.jpeg',
+    desc: 'vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats',
+  },
+  {
+    id: 3,
+    title: 'godzilla milkshakes',
+    category: 'shakes',
+    price: 6.99,
+    img: './images/item-3.jpeg',
+    desc: 'ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.',
+  },
+  {
+    id: 4,
+    title: 'country delight',
+    category: 'breakfast',
+    price: 20.99,
+    img: './images/item-4.jpeg',
+    desc: 'Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut,',
+  },
+  {
+    id: 5,
+    title: 'hamburger',
+    category: 'lunch',
+    price: 13.99,
+    img: './images/item-5.jpeg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores ut iusto quas quia dignissimos ullam. Enim voluptas, expedita architecto, cupiditate molestias quisquam error quam blanditiis tempore quo dicta. Ipsa.',
+  },
+  {
+    id: 6,
+    title: 'godzilla milkshake',
+    category: 'dinner',
+    price: 13.99,
+    img: './images/item-7.jpeg',
+    remote_img: '',
+    desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores ut iusto quas quia dignissimos ullam. Enim voluptas, expedita architecto, cupiditate molestias quisquam error quam blanditiis tempore quo dicta. Ipsa.',
+  },
+];
+
+export default menu_xx;

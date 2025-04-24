bef60370ba118af5827c34515bf89f92b9eb58d0
Zihyin <jenny010328@gmail.com>
2025-04-24 18:47:51 +0800

    W10-P1: Copy mid1 m3_xx answer and rearrange it for W10 demo

---
+++ demo/w10_mid1_m3/js/p3_xx.js
@@ -0,0 +1,31 @@
+import { mid_products_xx } from './products_data_xx.js';
+
+const productContainer = document.querySelector('.products-container');
+
+console.log('mid_products_xx', mid_products_xx);
+
+const displayProducts = (products) => {
+  let productsContent = products
+    .map((product) => {
+      const { id, title, price, img } = product;
+      return `
+        <div class="single-product">
+        <img
+          src=${img}
+          class="single-product-img img"
+          alt=${title}
+        />
+        <footer>
+          <h3 class="name">${id}-${title}</h3>
+          <span class="price">$${price}</span>
+        </footer>
+      </div>
+    `;
+    })
+    .join('');
+  productContainer.innerHTML = productsContent;
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  displayProducts(mid_products_xx);
+});

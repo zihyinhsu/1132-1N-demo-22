9adc6e1f6d01e2e2f9ca042f6d634903f3375e78
Zihyin Hsu <jenny010328@gmail.com>
2025-05-04 15:42:18 +0800

    W11-P2: Asynchronous DOM demo

---
+++ demo/w11_async_js_22/3-dom-cb-example/app_22.js
@@ -0,0 +1,20 @@
+const heading1 = document.querySelector('.one');
+const heading2 = document.querySelector('.two');
+const heading3 = document.querySelector('.three');
+const heading4 = document.querySelector('.four');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  setTimeout(() => {
+    heading1.style.color = 'red';
+    setTimeout(() => {
+      heading2.style.color = 'green';
+      setTimeout(() => {
+        heading3.style.color = 'blue';
+        setTimeout(() => {
+          heading4.style.color = 'purple';
+        }, 500);
+      }, 1000);
+    }, 2000);
+  }, 1000);
+});

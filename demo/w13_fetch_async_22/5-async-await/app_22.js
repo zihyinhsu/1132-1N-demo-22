9a910d9be16b843fddb42d8236e0ab64f92d7f86
Zihyin <jenny010328@gmail.com>
2025-05-15 19:47:22 +0800

    W13-P2: Use Async Await to replace fetch API approach

---
+++ demo/w13_fetch_async_22/5-async-await/app_22.js
@@ -0,0 +1,39 @@
+const button = document.querySelector('.btn');
+const url = './api/person.json';
+button.addEventListener('click', () => {
+    getDataAsyncAwait(url); 
+})
+
+const getDataAsyncAwait = async (url) => {
+    try {
+        const response = await fetch(url);  
+        const data = await response.json(); 
+        console.log('data', data);          
+        displayItems(data);                 
+    } catch (err) {
+        console.error('Error occurred:', err); 
+    }
+};
+
+const getDataByFetchAPI2 = (url) => {
+    fetch(url)
+        .then((response) => response.json()) 
+        .then((data) => {
+            console.log('data', data);       
+            displayItems(data);              
+        })
+        .catch((err) => console.error('Error occurred:', err)); 
+};
+
+const displayItems = (persons) => {
+    const displayData = persons
+        .map((person) => {
+            return `<p>${person.name}</p>`;
+        })
+        .join(''); // 連接所有 HTML 字符串
+
+    const element = document.createElement('div');
+    element.innerHTML = displayData;
+
+    document.body.appendChild(element);
+};

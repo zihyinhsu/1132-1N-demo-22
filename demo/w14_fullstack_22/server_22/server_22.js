63ecc200ef6e37eda79f4072e8db7dc5c1799273
Zihyin <jenny010328@gmail.com>
2025-05-25 22:52:48 +0800

    W14-P2: Show static products page

--- demo/w14_fullstack_22/server_22/server_22.js
+++ demo/w14_fullstack_22/server_22/server_22.js
@@ -5,6 +5,14 @@ const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.use('/product_22/static', (req, res, next) => {
+    res.render('product_22/static_22', {
+        title: 'Get Products - Static',
+        name: 'Zihyin Hsu',
+        id: '913410022',
+    });
+})
+
 app.use('/', (req, res, next) => {
     res.render('index', {
         title: 'Express',
